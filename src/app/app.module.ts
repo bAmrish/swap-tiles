2f4171c5ea81b678a5efdb6a8da0b9cc9087a3de
Amrish Bharatiya <amrish.bharatiya@aurigininc.com>
2021-06-08 13:45:40 +0530

    Moved the game logic to game component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {GameComponent} from './game/game.component';
 import {TileContainerComponent} from './tile-container/tile-container.component';
 import {BlankTileComponent} from './tile/blank-tile.component';
 import {TileComponent} from './tile/tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent, TileComponent, TileContainerComponent, BlankTileComponent
+    AppComponent, GameComponent, TileContainerComponent, TileComponent, BlankTileComponent
   ],
   imports: [
     BrowserModule
