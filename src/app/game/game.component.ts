ce493e4d69283f5426f1ef0d5e54e65a75650b96
Amrish Bharatiya <amrish.bharatiya@aurigininc.com>
2021-06-09 18:37:13 +0530

    initial updated code to support multi dimensional playboard

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,46 +7,46 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-  seedNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  seedNumbers: number[] = [];
+  dimension = 0;
+  neighbours: Record<number, number[]> = {};
   startNumbers: number[] = [];
   numbers: number[] = [];
   moves = 0;
   gameWon = false;
-  BLANK_TILE = 9;
+  BLANK_TILE = 0;
 
 
   constructor(private route: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit() {
+
     setTimeout(() => {
       this.route.queryParams.subscribe(params => {
-        const n = params['n'];
-        console.log('n = ', n);
-        let numbers: number[] = [];
-        if (n) {
-          let isValid = true;
-          for (let i = 1; i <= 9; i++) {
-            if (n.indexOf(i.toString()) == -1) {
-              isValid = false;
-              break;
-            }
-          }
-          if (isValid) {
-            numbers = n.toString().split("").map((n: string) => parseInt(n))
-          }
-        }
-        this.newGame(numbers);
+        this.newGame(...this.getDataFromQuery(params['n']));
       })
     }, 0)
 
   }
 
-  newGame = (numbers: number[] | null) => {
+  newGame = (numbers: number[] | null, dimension: number) => {
     this.moves = 0;
     this.gameWon = false;
     this.numbers = [];
-    if (!numbers || numbers.length == 0) {
+
+    if (!dimension || dimension < 3 || dimension > 5) {
+      this.dimension = 3;
+    } else {
+      this.dimension = dimension;
+    }
+    this.neighbours = this.findNeighbours(this.dimension);
+    this.seedNumbers =
+      [...new Array(this.dimension * this.dimension)]
+        .map((_, i) => i + 1);
+    this.BLANK_TILE = this.dimension * this.dimension;
+
+    if (!numbers || numbers.length == 0 || numbers.length != dimension * dimension) {
       const startNumbers = this.seedNumbers.slice(0);
       while (startNumbers.length != 0) {
         const pick = Math.random() * startNumbers.length;
@@ -55,14 +55,17 @@ export class GameComponent implements OnInit {
       }
     } else {
       this.numbers = numbers;
+      this.dimension = dimension;
     }
+
     this.router.navigate(
       [],
       {
         relativeTo: this.route,
-        queryParams: {n: this.numbers.join("")},
+        queryParams: {n: this.numbers.join(",")},
         queryParamsHandling: 'merge' // remove to replace all query params by provided
       }).then();
+
     this.startNumbers = this.numbers.slice(0);
   }
 
@@ -88,6 +91,39 @@ export class GameComponent implements OnInit {
     }
   }
 
+  private getDataFromQuery(data: string): [number[] | null, number] {
+    let isValid = true;
+    let dimension = 3;
+    let numbers: number[] = [];
+    if (data) {
+      numbers = data.split(",")
+        .map((d: string) => parseInt(d, 10))
+        .filter((c: number) => !isNaN(c));
+
+      const sqrt = Math.floor(Math.sqrt(numbers.length));
+
+      if (sqrt * sqrt === numbers.length) { // its a perfect square
+        dimension = sqrt;
+
+        for (let i = 1; i <= dimension; i++) {
+          if (numbers.indexOf(i) == -1) {
+            isValid = false;
+            break;
+          }
+        }
+      } else {
+        isValid = false;
+      }
+    } else {
+      isValid = false;
+    }
+    if (isValid) {
+      return [numbers, dimension];
+    } else {
+      return [null, dimension];
+    }
+  }
+
   private isGameWon() {
     let won = true;
     for (let index = 0; index < this.numbers.length; index++) {
@@ -105,27 +141,33 @@ export class GameComponent implements OnInit {
 
   // noinspection JSMethodCanBeStatic
   private canSwap(posNumber: number, posBlank: number): boolean {
-    switch (posNumber) {
-      case 0:
-        return posBlank == 1 || posBlank == 3;
-      case 1:
-        return posBlank == 0 || posBlank == 2 || posBlank == 4;
-      case 2:
-        return posBlank == 1 || posBlank == 5;
-      case 3:
-        return posBlank == 0 || posBlank == 4 || posBlank == 6;
-      case 4:
-        return posBlank == 1 || posBlank == 3 || posBlank == 5 || posBlank == 7;
-      case 5:
-        return posBlank == 2 || posBlank == 4 || posBlank == 8;
-      case 6:
-        return posBlank == 3 || posBlank == 7;
-      case 7:
-        return posBlank == 4 || posBlank == 6 || posBlank == 8;
-      case 8:
-        return posBlank == 5 || posBlank == 7;
-      default:
-        return false
+    return this.neighbours[posNumber].indexOf(posBlank) != -1;
+  }
+
+  // noinspection JSMethodCanBeStatic
+  private findNeighbours(d: number): Record<number, number[]> {
+    const allNeighbours: Record<number, number[]> = {};
+    for (let i = 0; i < d * d; i++) {
+      const neighbours: number[] = [];
+      const row = Math.floor(i / d);
+      const col = i % d;
+
+      if (col !== 0) {
+        neighbours.push(i - 1);
+      }
+      if (col !== d - 1) {
+        neighbours.push(i + 1);
+      }
+      if (row !== 0) {
+        neighbours.push((d * (row - 1)) + col);
+      }
+      if (row !== d - 1) {
+        neighbours.push((d * (row + 1)) + col);
+      }
+
+      allNeighbours[i] = neighbours;
+      //console.log(`${i} ${neighbours.sort((a, b) => a - b).join()}`);
     }
+    return allNeighbours;
   }
 }
